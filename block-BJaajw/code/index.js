a00341ba119ecbc5a3a58629e1e5487b6d632fab
keshavalt22 <keshavkhatri35@gmail.com>
2022-04-05 01:04:25 +0530

    assignment

---
+++ block-BJaajw/code/index.js
@@ -0,0 +1,70 @@
+let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30/`;
+let newsElm = document.querySelector('.news');
+let select = document.querySelector('select');
+let allNews = [];
+
+
+function renderNews(news){
+    newsElm.innerHTML = '';
+    news.forEach(newsItem => {
+        let li = document.createElement('li');
+        li.classList.add('flex');
+        let img = document.createElement('img');
+        img.src = newsItem.imageUrl;
+        img.alt = newsItem.title;
+        let div = document.createElement('div');
+        let span = document.createElement('span');
+        span.innerText = newsItem.newsSite;
+        let h3 = document.createElement('h3');
+        h3.innerText = newsItem.title;
+        let a = document.createElement('a');
+        a.href = newsItem.url;
+        let button = document.createElement('button');
+        button.innerText = 'Read More';
+        a.append(button);
+        div.append(span, h3, a);
+        li.append(img, div);
+        newsElm.append(li);
+    });
+} 
+
+function displayOptions(sources) {
+    sources.forEach(source => {
+        let option = document.createElement('option')
+        option.innerText = source;
+        option.value = source;
+        select.append(option)
+    })
+}
+
+fetch(url)
+.then(res =>  {
+    console.log(res);
+    if(!res.ok) {
+        throw new Error(`Error happened: ${res.status}`)
+    }
+    return res.json()
+})
+.then((news) => {
+    allNews = news;
+    renderNews(news);
+    let allSources = Array.from(new Set(news.map((n) => n.newsSite)));
+    displayOptions(allSources);
+})
+.catch((error) => {
+    let h3 = document.createElement('h3');
+    h3.innerText = error; 
+    newsElm.append(h3);
+});
+
+
+select.addEventListener('change', (event) => {
+    let source = event.target.value;
+
+    if(source) {
+        var filterNews = allNews.filter(news => news.newsSite === source)
+    }else {
+        filterNews = allNews;
+    }
+    renderNews(filterNews);
+});
